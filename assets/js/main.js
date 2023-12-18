ae28f97974d3a7e23154fc123193f797dfc7784f
Zamil <muzamil-javascriptdev@outlook.com>
2023-12-19 02:16:44 +0500

    updared

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,61 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const navLink = document.querySelectorAll('.nav-link');
+const backdrop = document.querySelector(".nav-menu-backdrop")
+const body = document.querySelector("body");
 
 hamburger.addEventListener('click', mobileMenu);
-navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
 function mobileMenu() {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  body.style.overflow="hidden";
+  backdrop.style.display="block"
 }
 
 function closeMenu() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  body.style.overflow="auto";
+  backdrop.style.display="none"
 }
 
 document.addEventListener('click', (event) => {
-  if (!navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+  if (!navMenu.contains(event.target) && !hamburger.contains(event.target) && backdrop.contains(event.target) ) {   
     closeMenu();
   }
 });
+
+//Dropdown
+const dropBtns = document.querySelectorAll(".dropdown-btn");
+
+if(screen.width < 768){
+  dropBtns[0].classList.toggle("active");
+}
+if(screen.width > 768){
+  document.addEventListener('click', (event) => {
+    if (!navMenu.contains(event.target) ) {   
+      var actBtn = document.querySelector(".dropdown-btn.active");
+      if(actBtn){
+        actBtn.classList.remove("active")
+      }
+    }
+  });
+}
+
+dropBtns.forEach(btn => btn.addEventListener("click",(event)=>{
+  if(screen.width > 768){
+    var actBtn = document.querySelector(".dropdown-btn.active");
+    if(actBtn == btn){
+      actBtn.classList.remove("active")
+    }
+    else {
+      if(actBtn) actBtn.classList.remove("active");
+      btn.classList.toggle("active");
+    } 
+  }
+  else  btn.classList.toggle("active");
+}))
 
 //Accordion Script
 var acc = document.getElementsByClassName('accordion');
