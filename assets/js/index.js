0e1c050645343a714b824fe7884ad20cb75d93c3
Zamil <muzamil-javascriptdev@outlook.com>
2023-12-19 19:46:33 +0500

    code formatted with prettier

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,23 @@
 var swiper = new Swiper('.insurers-swiper', {
-    a11y: false,
-    freeMode: true,
-    slidesPerView: 'auto',
-  });
+  a11y: false,
+  freeMode: true,
+  slidesPerView: 'auto',
+});
 
-  var swiper = new Swiper('.featured-swiper', {
-    a11y: false,
-    freeMode: true,
-    slidesPerView: 'auto',
-  });
+var swiper = new Swiper('.featured-swiper', {
+  a11y: false,
+  freeMode: true,
+  slidesPerView: 'auto',
+});
 
-  var swiper = new Swiper('.testm-swiper', {
-    a11y: false,
-    freeMode: true,
-    slidesPerView: 'auto',
-    spaceBetween:50,
-    breakpoints: {
-      1024: {
-        spaceBetween:90,
-      },
+var swiper = new Swiper('.testm-swiper', {
+  a11y: false,
+  freeMode: true,
+  slidesPerView: 'auto',
+  spaceBetween: 50,
+  breakpoints: {
+    1024: {
+      spaceBetween: 90,
+    },
   },
-   
-
-  });
+});
